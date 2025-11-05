e8f4a192cce9b1b996c2f26172e887cc4431e6a1
nia <ihor.nahornyy@gmail.com>
2025-11-05 14:40:01 +0200

    убрал рекламу добавил номер заменил фото на рокоссовского

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /opt/hugo/n.avtomoto25.cn.ua && hugo -s prebuild --ignoreCache --cleanDestinationDir && hugo  --cleanDestinationDir --minify && rsync -avzhe ssh /opt/hugo/n.avtomoto25.cn.ua/public/ root@192.168.50.105:/www/wwwroot/new.avtomoto25.cn.ua --delete
+cd /opt/hugo/n.avtomoto25.cn.ua && hugo --cleanDestinationDir -s prebuild --ignoreCache && hugo  --cleanDestinationDir --minify && rsync -avzhe ssh /opt/hugo/n.avtomoto25.cn.ua/public/ root@192.168.50.105:/www/wwwroot/new.avtomoto25.cn.ua --delete
